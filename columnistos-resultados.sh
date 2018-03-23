ead4971f20997a0c3d6d7ac73e5e93e678a9ba7c
Lu Pa <lupa@inventati.org>
2018-03-23 11:53:26 -0300

    script corregido ahora da la opcion de subir o no los resultados

---
+++ columnistos-resultados.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Script que saca los resultados y opcionalmente lo copia a una carpeta remota dav
+# NOTA: para que esto funcion hay que instalar davfs y configurar las credenciales
+
+# Variables
+carpetascript="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+carpeta=$carpetascript/columnistos-resultados
+subir=n
+
+# Obtengo resultados
+echo -e "Sacando resultados"
+docker-compose -f $carpetascript/docker-compose.yml run --rm app sqlite3 \
+	-header -csv /usr/src/app/diarios/diarios.sqlite \
+	"select * from articles a join authors aut where a.author_id = aut.id;" > \
+	$carpetascript/$(date +%Y%m%d_%H)_articulos.csv
+
+# Subimos?
+echo -e "------------------------------------------------"
+echo -n "Desea subir el resultado a davfs (s/n): "
+read subir
+echo -e "------------------------------------------------"
+if [ "$subir" = "s" ]; then
+
+  # Creo carpeta para montar davfs
+  mkdir $carpeta || exit
+
+  # Monto carpeta remota
+  echo -e "Montando carpeta remota"
+  sudo mount -t davfs https://nube.tedic.net/remote.php/webdav/TEDIC/ColumnistosResultados $carpeta/
+  sudo chown lupa:lupa $carpeta
+
+  # Copio y limpio
+  echo -e "Subiendo..."
+  mv $carpetascript/$(date +%Y%m%d_%H)_articulos.csv $carpeta
+  sleep 1
+
+  #Desmonto carpeta
+  echo -e "Desmontando carpeta remota"
+  sudo umount $carpeta/
+
+  #Limpiamos carpeta
+  sleep 1
+  rmdir $carpeta
+else
+  echo -e "\nNo se copia remotamente, el archivo está en:"
+  ls -lh $carpetascript/$(date +%Y%m%d_%H)_articulos.csv
+  sleep 2
+fi
