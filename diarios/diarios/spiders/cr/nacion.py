41032d3955fc66e48a6573120cbe0095e96d5735
gabygarro <ggarroab@gmail.com>
2018-04-08 15:43:10 -0600

    arreglo en nacion scrawler

--- diarios/diarios/spiders/cr/nacion.py
+++ diarios/diarios/spiders/cr/nacion.py
@@ -4,17 +4,17 @@ from scrapy.loader import ItemLoader
 
 from diarios.items import DiariosItem
 
+import logging
+
 
 class NacionSpider(scrapy.Spider):
     name = 'nacion'
     allowed_domains = ['www.nacion.com']
-    start_urls = [
-        'http://www.nacion.com/opinion/columnistas/'
-    ]
+    start_urls = ['http://www.nacion.com/opinion/columnistas/']
 
     def parse(self, response):
         """
-        @url http://www.nacion.com/
+        @url http://www.nacion.com/opinion/columnistas/
         @returns items 1 15
         @returns requests 0 0
         @scrapes author title url
@@ -24,12 +24,23 @@ class NacionSpider(scrapy.Spider):
             yield self.parse_article(selector, response)
 
     def parse_article(self, selector, response):
-        loader = ItemLoader(DiariosItem(), selector=selector)
+        import re
 
-        loader.add_xpath('title', './/article//figure/a/h4/text()')
-        loader.add_xpath(
-            'author',
-            './/article//figure/div/span[@class="autor"]/a/text()'
-        )
-        loader.add_xpath('url', './/article//figure/a[@href]')
+        loader = ItemLoader(item=DiariosItem(), selector=selector)
+
+        # autor = selector.xpath('./article//figure/div[@class="byline"]/span[@class="autor"]/a/text()')
+        # autor = re.sub('[^a-zA-ZñÑáéíóúÁÉÍÓÚ ]', '', autor)
+        # loader.add_value('author', autor)
+
+        titulo = selector.xpath('./article//figure/a/h4/text()').extract_first()
+        titulo = re.sub('[^a-zA-ZñÑáéíóúÁÉÍÓÚ ]', '', titulo)
+        loader.add_value('title', titulo)
+
+        autor = selector.xpath('./article//figure//div[@class="byline"]/span[@class="author"]//text()').extract_first()
+        autor = re.sub('[^a-zA-ZñÑáéíóúÁÉÍÓÚ ]', '', autor)
+        loader.add_value('author', autor)
+        #loader.add_xpath('author', './/article//figure//div[@class="byline"]//span[@class="author"]/a/text()')
+
+        loader.add_xpath('url', './article//figure/a/@href')
+
         return loader.load_item()
