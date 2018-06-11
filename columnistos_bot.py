5106fe510a2bfc3d00923a2c7238eb30eac5814f
Felipe Álvarez <falvarez@ciudadanointeligente.org>
2018-06-11 14:12:21 -0300

    Agregando un days None en cualquier caso, me acaba de ocurrir que no estaba referenciado

--- columnistos_bot.py
+++ columnistos_bot.py
@@ -419,6 +419,8 @@ def get_stats(site):
                 '%Y-%m-%dT%H:%M:%S%z')
             last_seen = last_seen.replace(tzinfo=None)
             days = yesterday - last_seen
+        else:
+            days = None
     else:
         days = yesterday - yesterday
     return dict(total=total, fem=fem, var=total-fem, days=days.days,
