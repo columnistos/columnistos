f1ef1f21c1791609c3a62d88f926d071558d0532
gabygarro <ggarroab@gmail.com>
2018-08-12 18:21:19 -0600

    remove print

--- columnistos_bot.py
+++ columnistos_bot.py
@@ -456,7 +456,6 @@ def main():
 
     # keys
     consumer_key = os.environ['TWITTER_CONSUMER_KEY']
-    print(os.environ['TWITTER_CONSUMER_KEY'])
     consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
     access_token = os.environ['TWITTER_ACCESS_TOKEN']
     access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
