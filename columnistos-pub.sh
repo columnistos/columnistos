acf10dbc3f40b858106d213a5dc3ada6c2d8be56
Lu <lupa@inventati.org>
2018-08-16 15:59:20 -0400

    agrego script para exponer bbdd en carpeta public

---
+++ columnistos-pub.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script que hace lo siguiente: 
+#   1. Saca los resultados
+#   2. Los expone como CSV en la carpeta pub
+#
+#   OBS: Mantiene un solo archvo que se sobreescribe cada ejecución
+
+# Variables
+pais=paraguay
+carpetascript="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+carpeta=$carpetascript/public
+
+# creo carpeta por si no existe
+mkdir -p $carpeta
+
+# Obtengo resultados
+echo -e "Sacando resultados"
+echo -e "docker-compose -f $carpetascript\/docker-compose.yml run \-\-rm app sqlite3 -header -csv \/usr\/src\/app\/diarios\/diarios.sqlite \"select * from articles a join authors aut where a.author_id = aut.id;\" \> $carpeta/$pais_articulos.csv"
+docker-compose -f $carpetascript/docker-compose.yml run --rm app sqlite3 \
+	-header -csv /usr/src/app/diarios/diarios.sqlite \
+	"select * from articles a join authors aut where a.author_id = aut.id;" > \
+	$carpeta/$pais-articulos.csv
+
