a17696e3ab10f40767c0a43ee8a7bb022f11e902
jed <j-e-d@users.noreply.github.com>
2019-09-06 17:08:50 -0300

    Cambios pedidos

--- diarios/diarios/spiders/py/abc.py
+++ diarios/diarios/spiders/py/abc.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+import re
 
 import scrapy
 from scrapy.loader import ItemLoader
@@ -35,8 +36,16 @@ class AbcSpider(scrapy.Spider):
         loader = ItemLoader(DiariosItem())
         for cred in item['credits']['by']:
             if cred['type'] == 'author':
-                autor = cred['name'].title()
+                autor = cred['name'].title().strip()
+                # Busco la coma
+                poscoma = autor.find(',')
+                # Si hay coma me quedo con lo de la izquierda
+                if  poscoma > -1:
+                    autor = autor[:poscoma]
+                # Saco símbolos extraños
+                autor = re.sub('[^a-zA-ZñÑáéíóúÁÉÍÓÚ ]', '', autor).strip()
         loader.add_value('author', autor)
         loader.add_value('title', item['headlines']['basic'])
-        loader.add_value('url', item['website_url'])
+        loader.add_value('url',
+                         'https://www.abc.com.py/' + item['website_url'])
         return loader.load_item()
